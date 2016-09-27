c1571a7c7cf400d11edc5162393c1e4fca9c1474
Adam Wysknski <sigmund.messner@gmail.com>
2016-09-27 04:06:07 +0200

    Fix typos, add Controller and root route

---
+++ app/controllers/root_controller.rb
@@ -0,0 +1,8 @@
+class RootController < ApplicationController
+
+  def index
+    render json: {
+      
+    }
+  end
+end
--- config/initializers/active_model_serializer.rb
+++ config/initializers/active_model_serializer.rb
@@ -1,2 +1,1 @@
-ActiveModel::Serializer::Adapter.register(:json_api_dasherized, JsonApiDasherized)
 ActiveModel::Serializer.config.adapter = :json_api_dasherized
