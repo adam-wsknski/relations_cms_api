0a9e9c56cf02628bb38cb2067ad381949cd1c101
Adam Wysknski <sigmund.messner@gmail.com>
2016-09-27 12:24:21 +0200

    add relations in project, offer, task serializers and contact model

--- app/serializers/offer_serializer.rb
+++ app/serializers/offer_serializer.rb
@@ -1,3 +1,10 @@
 class OfferSerializer < ActiveModel::Serializer
+  enum status: [:active, :accepted, :rejected, :expired]
+
+  validates :name, presence: true
+
   attributes :id, :name, :price, :status, :valid_until
+  has_many :tasks
+  has_and_belongs_to_many :contacts
+  belongs_to :project
 end
