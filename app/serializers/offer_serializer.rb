1968e53e97320fd9b1dc18cee84df114f2a4474a
Adam Wysknski <sigmund.messner@gmail.com>
2016-09-27 14:02:39 +0200

    working out proper relations within both models and serializers

--- app/serializers/offer_serializer.rb
+++ app/serializers/offer_serializer.rb
@@ -1,10 +1,7 @@
 class OfferSerializer < ActiveModel::Serializer
-  enum status: [:active, :accepted, :rejected, :expired]
-
-  validates :name, presence: true
-
   attributes :id, :name, :price, :status, :valid_until
   has_many :tasks
-  has_and_belongs_to_many :contacts
-  belongs_to :project
+  has_many :contacts
+  has_many :notes
+  has_one :project
 end
