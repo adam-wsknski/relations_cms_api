fa6a41eb9c9c9d40ce0ce294f2e8145bee30b1e4
Adam Wysknski <sigmund.messner@gmail.com>
2016-09-27 05:03:35 +0200

    Fix validation in contact model, add errors msg according to the json_api spec

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -2,5 +2,5 @@ class Contact < ApplicationRecord
   belongs_to :company
 
   validates :family_name, presence: true
-            :given_names, presence: true
+  validates :given_names, presence: true
 end
