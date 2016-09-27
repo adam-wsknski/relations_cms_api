e8661bf46e505545914466a86a05a53f7eec386f
Adam Wysknski <sigmund.messner@gmail.com>
2016-09-27 04:39:09 +0200

    Add contact scaffold

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160927023253) do
+ActiveRecord::Schema.define(version: 20160927023824) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name"
@@ -22,6 +22,22 @@ ActiveRecord::Schema.define(version: 20160927023253) do
     t.text     "additional_info"
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
+  end
+
+  create_table "contacts", force: :cascade do |t|
+    t.string   "family_name"
+    t.string   "given_names"
+    t.integer  "company_id"
+    t.string   "title"
+    t.string   "phone"
+    t.string   "email"
+    t.string   "website"
+    t.text     "address"
+    t.string   "customer_id"
+    t.text     "additional_info"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+    t.index ["company_id"], name: "index_contacts_on_company_id"
   end
 
 end
